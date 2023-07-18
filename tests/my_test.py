45cd83158f2d6d3a466b4b4d663b6370d7c1cb09
Ofir <ofirfirst@gmail.com>
2023-07-18 14:37:03 +0300

    Final commit

--- tests/my_test.py
+++ tests/my_test.py
@@ -1,5 +1,12 @@
 import unittest
 import functions
+import API
+
+
+dollar = API.get_data()[0]
+shekel = API.get_data()[2]
+eur = API.get_data()[1]
+
 
 class MyTest(unittest.TestCase):
 
@@ -7,26 +14,29 @@ class MyTest(unittest.TestCase):
     def setUpClass(self):
         print('hello')
 
-    # def test_03(self):
-    #     assert MyTest.eur > MyTest.dollar
-    #
-    # def test_02(self):
-    #     assert MyTest.dollar > MyTest.shekel
+    def test_03(self):
+        assert eur > dollar
+
+    def test_02(self):
+        assert dollar > shekel
 
 
     def test_04(self):
         data = functions.make_file_into_list()
         for line in data:
             try:
-                checking_results = float(line[3]) * float(line[1])
+                if int(line[1]) > 4:
+                    checking_results = float(line[3]) / float(line[1])
+                else:
+                    checking_results = float(line[3]) * float(line[1])
                 assert checking_results == float(line[0])
             except ValueError:
                 continue
 
-    # def test_01(self):
-    #     assert MyTest.shekel1 > MyTest.shekel
-    #
-    # @classmethod
-    # def tearDownClass(self):
-    #     print(f"Dollar rate: {MyTest.dollar/MyTest.shekel}, EUR rate: {MyTest.eur/MyTest.shekel},"
-    #           f" shekel rate for usd: {MyTest.shekel/MyTest.dollar}, shekel rate for eur: {MyTest.shekel}")
+    def test_01(self):
+        assert eur > shekel
+
+    @classmethod
+    def tearDownClass(self):
+        print(f"Dollar rate: {dollar}, EUR rate: {eur},"
+              f" shekel rate: {shekel}")
